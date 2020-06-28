10c39a2281026154123f50c856f521346c90d590
OlegGromOff <rekops@ukr.net>
2020-06-28 13:11:52 +0300

    finish project2

---
+++ build/precache-manifest.35759e89dcd65f9d5db688cd90c50d4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08d3193c0a432e866e0d099948900a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4d0f15e676208acdfc7",
+    "url": "/static/css/main.7b030056.chunk.css"
+  },
+  {
+    "revision": "6cad53c0255961f4bb7a",
+    "url": "/static/js/2.e65d2de5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e65d2de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d0f15e676208acdfc7",
+    "url": "/static/js/main.f116a6d8.chunk.js"
+  },
+  {
+    "revision": "3b54ffd57b5da78865d0",
+    "url": "/static/js/runtime-main.ba04576a.js"
+  }
+]);
